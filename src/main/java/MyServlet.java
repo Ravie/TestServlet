8ca7ced2006761819d0ef6ff5fe05c22e433b92c
Ravie <ravijkee@yandex.ru>
2019-04-21 17:53:36 +0300

    Using WebServlet annotation for mapping

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,4 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/h")
 public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        String message = "Hello from doGet() method of HttpServlet";
+        writer.println(message);
+        writer.close();
+    }
 }
